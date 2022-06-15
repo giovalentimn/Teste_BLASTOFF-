4529b344808d2b78479d3247921b0a65583a047e
Giovanna Dantas <92923479+giovalentimn@users.noreply.github.com>
2022-06-15 10:21:26 -0400

    Update and rename Q9.js

---
+++ questão 9/Q9.js
@@ -0,0 +1,9 @@
+var variavel1 = 'Hello ';
+var variavel2 = 'World!<br><br>'; 
+var variavel3 = String();
+
+variavel3 = "Toodoo"
+
+var final_text = variavel1 + variavel3;
+
+document.write(final_text);
